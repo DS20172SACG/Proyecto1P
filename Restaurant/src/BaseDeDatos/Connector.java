3441792392d74cef1205d3fd50423988b75c1c09
llullanqui <luis.1142@hotmail.com>
2018-01-13 17:02:37 -0500

    ..

--- Restaurant/src/BaseDeDatos/Connector.java
+++ Restaurant/src/BaseDeDatos/Connector.java
@@ -5,10 +5,36 @@
  */
 package BaseDeDatos;
 
+import java.sql.*;
+import javax.swing.JOptionPane;
+
 /**
  *
  * @author Usuario
  */
 public class Connector {
     
+    private Connector instancia = new Connector();
+    private Connection connection;
+    private DatosBase datos = new DatosBase();
+    
+    private Connector(){
+    }
+    
+    public Connector getInstancia(){
+        return this.instancia;
+    }
+    
+    public Connection getConnection() {
+        try {
+            connection = DriverManager.getConnection(datos.getUrl(), datos.getUser(), datos.getPassword());
+            //System.out.println("Se generó la conexion. ");
+        } catch (Exception ex) {
+            System.out.println("error occured " + ex.toString());
+            System.out.println("No se generó la conexion. ");
+            JOptionPane.showMessageDialog(null, "No se generó la conexion con la base de datos ","Mensaje del sistema",JOptionPane.ERROR_MESSAGE);
+            System.exit(0);
+        }
+        return connection;
+    }
 }
---
+++ Restaurant/src/BaseDeDatos/DatosBase.java
@@ -0,0 +1,51 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package BaseDeDatos;
+
+/**
+ *
+ * @author Usuario
+ */
+public class DatosBase {
+    
+    private String url;
+    private String user;
+    private String password;
+
+    public DatosBase() {
+    }
+
+    public DatosBase(String url, String user, String password) {
+        this.url = url;
+        this.user = user;
+        this.password = password;
+    }
+
+    public String getUrl() {
+        return url;
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
+
+    public String getUser() {
+        return user;
+    }
+
+    public void setUser(String user) {
+        this.user = user;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+    
+}
