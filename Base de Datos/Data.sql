754de5882831fa3d208e75934f6114fb23b25098
AnibalG <aagamboa@espol.edu.ec>
2018-01-14 16:12:57 -0500

    datos cargo

---
+++ Base de Datos/Data.sql
@@ -0,0 +1,7 @@
+use SARES;
+
+INSERT INTO Cargo (idCargo, nombreCargo, eliminado) VALUES (1, 'Administrador', 0);
+INSERT INTO Cargo (idCargo, nombreCargo, eliminado) VALUES (2, 'Cocinero', 0);
+INSERT INTO Cargo (idCargo, nombreCargo, eliminado) VALUES (3, 'Mesero', 0);
+INSERT INTO Cargo (idCargo, nombreCargo, eliminado) VALUES (4, 'Repartidor', 0);
+INSERT INTO Cargo (idCargo, nombreCargo, eliminado) VALUES (5, 'Cajero', 0);
