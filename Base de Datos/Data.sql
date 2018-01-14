01e887537ddaa9cbec89f48f61a0eedb3067b35e
AnibalG <aagamboa@espol.edu.ec>
2018-01-14 16:41:01 -0500

    datos mesa, ambiente, tipo de pago

--- Base de Datos/Data.sql
+++ Base de Datos/Data.sql
@@ -12,3 +12,20 @@ INSERT INTO Categoria_Articulo (ID, Nombre) VALUES (3, 'Postre');
 INSERT INTO Categoria_Articulo (ID, Nombre) VALUES (4, 'Refresco');
 INSERT INTO Categoria_Articulo (ID, Nombre) VALUES (5, 'Natural');
 INSERT INTO Categoria_Articulo (ID, Nombre) VALUES (6, 'Artificial');
+
+INSERT INTO TipoDePago (ID, Tipo) VALUES (1, 'Efectivo');
+INSERT INTO TipoDePago (ID, Tipo) VALUES (2, 'Visa');
+INSERT INTO TipoDePago (ID, Tipo) VALUES (3, 'Dinero Electronico');
+
+INSERT INTO Ambientes (idAmbiente, nombre, eliminado) VALUES (1, 'Normal', 0);
+INSERT INTO Ambientes (idAmbiente, nombre, eliminado) VALUES (2, 'VIP', 0);
+
+INSERT INTO Mesa (idMesa, asientos, disponibilidad, idAmbiente, eliminado) VALUES (1, 2, 1, 1, 0);
+INSERT INTO Mesa (idMesa, asientos, disponibilidad, idAmbiente, eliminado) VALUES (2, 2, 1, 1, 0);
+INSERT INTO Mesa (idMesa, asientos, disponibilidad, idAmbiente, eliminado) VALUES (3, 2, 1, 2, 0);
+INSERT INTO Mesa (idMesa, asientos, disponibilidad, idAmbiente, eliminado) VALUES (4, 4, 1, 1, 0);
+INSERT INTO Mesa (idMesa, asientos, disponibilidad, idAmbiente, eliminado) VALUES (5, 4, 1, 2, 0);
+INSERT INTO Mesa (idMesa, asientos, disponibilidad, idAmbiente, eliminado) VALUES (6, 6, 1, 1, 0);
+INSERT INTO Mesa (idMesa, asientos, disponibilidad, idAmbiente, eliminado) VALUES (7, 8, 1, 1, 0);
+
+
