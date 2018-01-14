aac6f701967ddbe46c122d73d65b13f2aabd4c56
AnibalG <aagamboa@espol.edu.ec>
2018-01-14 16:19:53 -0500

    datos cargo

--- Base de Datos/Data.sql
+++ Base de Datos/Data.sql
@@ -5,3 +5,10 @@ INSERT INTO Cargo (idCargo, nombreCargo, eliminado) VALUES (2, 'Cocinero', 0);
 INSERT INTO Cargo (idCargo, nombreCargo, eliminado) VALUES (3, 'Mesero', 0);
 INSERT INTO Cargo (idCargo, nombreCargo, eliminado) VALUES (4, 'Repartidor', 0);
 INSERT INTO Cargo (idCargo, nombreCargo, eliminado) VALUES (5, 'Cajero', 0);
+
+INSERT INTO Categoria_Articulo (ID, Nombre) VALUES (1, 'Entrada');
+INSERT INTO Categoria_Articulo (ID, Nombre) VALUES (2, 'Plato Fuerte');
+INSERT INTO Categoria_Articulo (ID, Nombre) VALUES (3, 'Postre');
+INSERT INTO Categoria_Articulo (ID, Nombre) VALUES (4, 'Refresco');
+INSERT INTO Categoria_Articulo (ID, Nombre) VALUES (5, 'Natural');
+INSERT INTO Categoria_Articulo (ID, Nombre) VALUES (6, 'Artificial');
