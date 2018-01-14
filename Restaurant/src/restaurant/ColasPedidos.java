e13e46a81dccfeaf5acd3f03d8c769511d35f4c3
edwardcruzcruz <edward.cruzcruz@hotmail.com>
2018-01-14 15:13:19 -0500

    Agredango nueva cola para cocinero y metodo donde notifique y agregue el pedido a esa cola

--- Restaurant/src/restaurant/ColasPedidos.java
+++ Restaurant/src/restaurant/ColasPedidos.java
@@ -7,40 +7,52 @@ package restaurant;
 
 import java.util.LinkedList;
 import java.util.Queue;
+import java.util.Vector;
+import restaurant.Personal.Observador;
 
 /**
  *
  * @author Usuario
  */
 public class ColasPedidos {
-    
+    private Vector Observadores = new Vector();
     private static ColasPedidos instancia = new ColasPedidos();
     private Queue<String> colaNormal;
     private Queue<String> colaPrioritaria;
+    private Queue<String> colaEntrega;
     
     private ColasPedidos(){
         colaNormal = new LinkedList<>();
         colaPrioritaria = new LinkedList<>();
+        colaEntrega = new LinkedList<>();
     }
     
     public void agregarPedidoNormal(String idPedido){
         colaNormal.offer(idPedido);
+        Observadores.addElement(idPedido);
     }
     
     public void agregarPedidoPrioritario(String idPedido){
         colaPrioritaria.offer(idPedido);
+        Observadores.addElement(idPedido);
     }
     
     public String atenderPedidoNormal(){
+        
         return colaNormal.poll();
     }
     
     public String atenderPedidoPrioritario(){
+        
         return colaNormal.poll();
+    }  
+    public void AgregarPedidoPorEntregar(String idPedido){
+         colaEntrega.offer(idPedido);
     }
-    
+    public String PedidoPorEntregado(){
+         return colaEntrega.poll();
+    }
     public static ColasPedidos getInstancia(){
         return instancia;
     }
-    
 }
