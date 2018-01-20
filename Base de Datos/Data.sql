21dc0115f587ff0629446241872c1cddcccf8c23
edwardcruzcruz <edward.cruzcruz@hotmail.com>
2018-01-20 16:07:04 -0500

    agrega valor a pedido, detalle_pedido,..

--- Base de Datos/Data.sql
+++ Base de Datos/Data.sql
@@ -14,8 +14,7 @@ VALUES	(1, 'Entrada'),
         (3, 'Postre'), 
         (4, 'Refresco'), 
         (5, 'Natural'),
-        (6, 'Artificial'),
-        (7, 'Combo');
+        (6, 'Artificial');
 
 INSERT INTO TipoDePago (ID, Tipo) 
 VALUES 	(1, 'Efectivo'),  
@@ -26,14 +25,14 @@ INSERT INTO Ambientes (idAmbiente, nombre, eliminado)
 VALUES 	(1, 'Normal', 0), 
 		(2, 'VIP', 0);
 
-INSERT INTO Mesa (idMesa, nombreMesa, asientos, disponibilidad, idAmbiente, eliminado) 
-VALUES 	(1,'Mesa 1', 2, 1, 1, 0), 
-		(2,'Mesa 2', 2, 1, 1, 0), 
-		(3,'Mesa 3', 2, 1, 2, 0), 
-        (4,'Mesa 4', 4, 1, 1, 0), 
-        (5,'Mesa 5', 4, 1, 2, 0), 
-        (6,'Mesa 6', 6, 1, 1, 0), 
-        (7,'Mesa 7', 8, 1, 1, 0);
+INSERT INTO Mesa (idMesa, asientos, disponibilidad, idAmbiente, eliminado) 
+VALUES 	(1, 2, 1, 1, 0), 
+		(2, 2, 1, 1, 0), 
+		(3, 2, 1, 2, 0), 
+        (4, 4, 1, 1, 0), 
+        (5, 4, 1, 2, 0), 
+        (6, 6, 1, 1, 0), 
+        (7, 8, 1, 1, 0);
 
 INSERT INTO Usuario (usuario, clave, eliminado)
 VALUES 	('admin', 'superclave', 0), 
@@ -51,17 +50,37 @@ VALUES 	('0900000000', 'Luis', 'Arizaga', 23, 500, 1, 'admin', 0),
         ('0981237645', 'Jose Jose', 'Primero Segundo', 19, 300, 3, 'elmesero2', 0),
         ('0987612345', 'Leonardo', 'Velez', 23, 325, 5, 'elcajero', 0);
 
-INSERT INTO Articulo (Nombre, Descripcion, Precio, Disponibilidad, TiempoPreparacion, Idcategoria, eliminado) 
-VALUES 	('limonada', 'jarra de limonada ', 4, 1, 3, 5, 0),
-		('Jugo de Durazno', 'Jugo Artificial', 1, 1, 0, 6, 0),
-		('Patacones', 'patacones con queso', 2, 1, 4, 1, 0),
-        ('seco de pollo', 'seco de pollo', 4, 1, 10, 2, 0),
-        ('Arroz marinero', 'arroz marinero', 7, 1, 13, 2, 0),
-        ('tres leches', 'tres leches', 3, 1, 4, 3, 0),
-        ('Flan','Flan de vainilla preparado con las mejores esencias', 1, 1, 2, 3, 0),
-        ('Almuerzo Playero','Contiene Arroz Marinero, limonada, y un suculento flan', 10, 1, 19, 7, 0);
+INSERT INTO Articulo (ID, Nombre, Descripcion, Precio, Disponibilidad, TiempoPreparacion, Idcategoria, eliminado) 
+VALUES 	(1, 'limonada', 'jarra de limonada ', 4, 1, 3, 5, 0),
+		(2, 'Jugo de Durazno', 'Jugo Artificial', 1, 1, 0, 6, 0),
+		(3, 'Patacones', 'patacones con queso', 2, 1, 4, 1, 0),
+        (4, 'seco de pollo', 'seco de pollo', 4, 1, 10, 2, 0),
+        (5, 'Arroz marinero', 'arroz marinero', 7, 1, 13, 2, 0),
+        (6, 'tres leches', 'tres leches', 3, 1, 4, 3, 0);
+#modificado---------
+INSERT INTO cliente (cedula,LastName, FirstName, Direccion,eliminado) 
+VALUES ('0936674845','Cruz','Edward','25 y G','0'),
+		('0986494824', 'Velez', 'Andres', '15  Colon', '0');
 
-INSERT INTO ArticulosCombos (idCombo,idArticulo)
-values (8,5),
-       (8,1),
-       (8,7);
+INSERT INTO pedido (id, pagado, enPreparacion, cocinado, entregado, EnCola, fecha, hora, idCliente, idMesero, idCocinero) 
+VALUES ('1', '1', '1', '1', '1', '0', '2018-01-19', '19:00', '0936674845', '0912873456', '0921234567'),
+		('2', '1', '1', '1', '1', '0', '2018-01-19', '20:00', '0986494824', '0981237645', '0921234567');
+INSERT INTO detalle_pedido (ID_detalle, ID_Pedido, ID_Articulo,cantidad, Observaciones) 
+VALUES ('1', '1', '5', '1', 'ninguna'),
+		('2','2','4','1','ninguna');
+
+INSERT INTO factura (ID, TOTAL, Fecha, Id_cliente, Descuento, TipoDePago) 
+VALUES ('1', '7', '2018-01-19', '0936674845', '0', '1'),
+		('2', '4', '2018-01-19', '0936674845', '0', '1');
+INSERT INTO detalle_factura (id_detalle, id_factura, id_pedido) 
+VALUES ('1', '1', '1'),
+		('2', '2', '2');
+#modificado---------
+
+
+
+        
+
+
+
+
