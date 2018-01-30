4631027b00b928ef1dd5bddaea9632eb10511b89
AnibalG <aagamboa@espol.edu.ec>
2018-01-29 20:31:15 -0500

    refactoring funcion addListener

--- Restaurant/src/Controladores/ControlCajero.java
+++ Restaurant/src/Controladores/ControlCajero.java
@@ -44,20 +44,51 @@ public class ControlCajero implements Controlador {
     }
     
     public void addListeners(){
-        ventana.getjButton3().addActionListener(this);
-        ventana.getjList1().addListSelectionListener((ListSelectionEvent e) -> {
-            ventana.getjTextField1().setText(ventana.getjList1().getSelectedValue().getCedula());
-            ventana.getjTextField3().setText(ventana.getjList1().getSelectedValue().getNombre());
-            ventana.getjTextField4().setText(ventana.getjList1().getSelectedValue().getApellido());
-            ventana.getjTextField5().setText(ventana.getjList1().getSelectedValue().getDireccion());
+        addListaClientesListener();
+        addActualizarListener();
+        addAgregarPedListener();
+        addQuitarPedListener();
+        addFacturarListener();
+    }
+
+    private void addFacturarListener() {
+        ventana.getFacturarBut().addActionListener((e) -> {
+            int desc = (Integer)ventana.getjSpinner1().getModel().getValue();
+            String TipoDePago = ventana.getjComboBox1().getSelectedItem().toString();
+            
+            if("Efectivo".equals(TipoDePago)){
+                PagoEfectivo pago = new PagoEfectivo();
+                pago.pagar(cliente, desc);
+            }
+            else if("Tarjeta Visa".equals(TipoDePago)){
+                PagoTarjetaVisa pago = new PagoTarjetaVisa();
+                pago.pagar(cliente, desc);
+            }
+            else if("Dinero Electrónico".equals(TipoDePago)){
+                PagoDineroElectronico pago = new PagoDineroElectronico();
+                pago.pagar(cliente, desc);
+            }
+            DefaultListModel<String> ListaPedidosPorPagar  = (DefaultListModel)ventana.getjList2().getModel();
+            ListaPedidosPorPagar.removeElement(cliente);
+            DefaultListModel<String> ListaPedidosEnFactura = (DefaultListModel)ventana.getjList3().getModel();
+            ListaPedidosEnFactura.removeAllElements();
+            
+            ventana.getjTextPane1().setText("Cod.   Articulo\tCant.   Precio");
+            
         });
-        
-        ventana.getjButton3().addActionListener((ActionEvent e) -> {
-            //actualizar cliente.
+    }
+
+    private void addQuitarPedListener() {
+        ventana.getQuitarPed().addActionListener((ActionEvent e) -> {
+
+            DefaultListModel<String> ListaPedidosEnFactura = (DefaultListModel)ventana.getjList3().getModel();
+            ListaPedidosEnFactura.removeAllElements();
+            
+            ventana.getjTextPane1().setText("Cod.   Articulo\tCant.   Precio");
         });
-        
-        
-        
+    }
+
+    private void addAgregarPedListener() {
         ventana.getAgregarPed().addActionListener((ActionEvent e) -> {
             cliente = ventana.getjList2().getSelectedValue();
             DefaultListModel<String> ListaPedidosPorPagar  = (DefaultListModel)ventana.getjList2().getModel();
@@ -71,47 +102,23 @@ public class ControlCajero implements Controlador {
                 ListaPedidosEnFactura.addElement(s);
                 text += Consultador.getInstancia().generarDetalleFactura(s);
             }
-            
-            
             ventana.getjTextPane1().setText(ventana.getjTextPane1().getText() + "\n"+ text);
         });
-        
-        ventana.getQuitarPed().addActionListener((ActionEvent e) -> {
-            
-            
-            
-            DefaultListModel<String> ListaPedidosEnFactura = (DefaultListModel)ventana.getjList3().getModel();
-            ListaPedidosEnFactura.removeAllElements();
-            
-            ventana.getjTextPane1().setText("Cod.   Articulo\tCant.   Precio");
+    }
+
+    private void addActualizarListener() {
+        ventana.getjButton3().addActionListener((ActionEvent e) -> {
+            //actualizar cliente.
         });
-        
-        ventana.getFacturarBut().addActionListener((e) -> {
-            int desc = (Integer)ventana.getjSpinner1().getModel().getValue();
-            String TipoDePago = ventana.getjComboBox1().getSelectedItem().toString();
-            
-           if("Efectivo".equals(TipoDePago)){
-               PagoEfectivo pago = new PagoEfectivo();
-               pago.pagar(cliente, desc);
-           }
-           else if("Tarjeta Visa".equals(TipoDePago)){
-               PagoTarjetaVisa pago = new PagoTarjetaVisa();
-               pago.pagar(cliente, desc);
-           }
-           else if("Dinero Electrónico".equals(TipoDePago)){
-               PagoDineroElectronico pago = new PagoDineroElectronico();
-               pago.pagar(cliente, desc);
-           }
-           DefaultListModel<String> ListaPedidosPorPagar  = (DefaultListModel)ventana.getjList2().getModel();
-           ListaPedidosPorPagar.removeElement(cliente);
-           DefaultListModel<String> ListaPedidosEnFactura = (DefaultListModel)ventana.getjList3().getModel();
-            ListaPedidosEnFactura.removeAllElements();
-            
-            ventana.getjTextPane1().setText("Cod.   Articulo\tCant.   Precio");
-            
+    }
+
+    private void addListaClientesListener() {
+        ventana.getjList1().addListSelectionListener((ListSelectionEvent e) -> {
+            ventana.getjTextField1().setText(ventana.getjList1().getSelectedValue().getCedula());
+            ventana.getjTextField3().setText(ventana.getjList1().getSelectedValue().getNombre());
+            ventana.getjTextField4().setText(ventana.getjList1().getSelectedValue().getApellido());
+            ventana.getjTextField5().setText(ventana.getjList1().getSelectedValue().getDireccion());
         });
-        
-        
     }
     
     
